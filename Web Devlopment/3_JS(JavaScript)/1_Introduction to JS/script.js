88f4926feabe2e9ad05953fc0d487a4ee2346bce
Vaishnavi Deshmukh <deshmukh.vaishnavi1410@gmail.com>
2025-04-04 11:23:05 +0530

    Add files via upload

---
+++ Web Devlopment/3_JS(JavaScript)/1_Introduction to JS/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+
+console.log("Code is Running....")
+
+console.log("Code is also Running....")
+
+console.log("Code is Running Like a WOW...")
+
+var a = prompt("Enter Your Number")
+
+var isTrue = prompt("Are you sure want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is Blasting")
+}
+
+else{
+    console.log("Computer is not Blasting")
+};
+
+console.log("Your number is " + a)
